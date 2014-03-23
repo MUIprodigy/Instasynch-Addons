f18b44a9d39025d19c9b890c9896166860482705
Bibbytube <megafaqqq@gmail.com>
2014-03-23 04:10:04 +0100

    Fixed logInOffMessages
    Fixed log in messages for every user in the room when joining a room

--- Chat Additions/logInOffMessages/logInOffMessages.js
+++ Chat Additions/logInOffMessages/logInOffMessages.js
@@ -8,18 +8,25 @@ setField({
     'section': 'Chat Additions'
 });
 
-
-function loadLogInOffMessages() {
-    events.bind('onAddUser', function(user) {
-        if (user.loggedin && GM_config.get('LogInOffMessages')) {
-            unsafeWindow.addMessage('', String.format('{0} logged on.', user.username), '', 'hashtext');
-        }
-    });
-    events.bind('onRemoveUser', function(id, user) {
-        if (user.loggedin && GM_config.get('LogInOffMessages')) {
-            unsafeWindow.addMessage('', String.format('{0} logged off.', user.username), '', 'hashtext');
-        }
-    });
+function userLoggedOn(user) {
+    if (user.loggedin && GM_config.get('LogInOffMessages')) {
+        unsafeWindow.addMessage('', String.format('{0} logged on.', user.username), '', 'hashtext');
+    }
 }
 
-events.bind('onExecuteOnce', loadLogInOffMessages);
+function userLoggedOff(id, user) {
+    if (user.loggedin && GM_config.get('LogInOffMessages')) {
+        unsafeWindow.addMessage('', String.format('{0} logged off.', user.username), '', 'hashtext');
+    }
+}
+
+function loadLogInOffMessages() {
+    events.bind('onAddUser', userLoggedOn);
+    events.bind('onRemoveUser', userLoggedOff);
+}
+
+events.bind('onResetVariables', function() {
+    events.unbind('onAddUser', userLoggedOn);
+    events.unbind('onRemoveUser', userLoggedOff);
+});
+events.bind('onPostConnect', loadLogInOffMessages);
